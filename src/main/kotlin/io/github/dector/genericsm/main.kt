a93eff1d9cf898636f4ae72411727b8264cf1704
Denys M <dector9@gmail.com>
2019-10-17 22:57:35 +0400

    Add ability to dry-run parse.

--- src/main/kotlin/io/github/dector/genericsm/main.kt
+++ src/main/kotlin/io/github/dector/genericsm/main.kt
@@ -6,7 +6,9 @@ fun main() {
     val environment = EnvironmentCfg(
         specificationsDir = File("data/problem-specifications"),
         assetsDir = File("data/assets"),
-        outDir = File("generated-exercises")
+        outDir = File("generated-exercises"),
+        skipFailed = true,
+        onlyParse = true
     )
 
     execute(environment)
@@ -15,12 +17,42 @@ fun main() {
 private fun execute(environment: EnvironmentCfg) {
     println("Loading exercises...")
 
+    val counters = object {
+        val succeeded = mutableListOf<String>()
+        val failed = mutableListOf<String>()
+    }
+
     loadExercisesDataFiles(environment)
         .also { println("  found ${it.size} exercises") }
         .asSequence()
         .onEach { println("Processing `${it.parentFile.name}`") }
-        .map(environment.parser::parse)
-        .forEach { generateExercise(environment, it) }
+        .mapNotNull { file ->
+            val parseResult = runCatching { environment.parser.parse(file) }
+                .onFailure { counters.failed += file.parentFile.name }
+                .onSuccess { counters.succeeded += file.parentFile.name }
+
+            val result = if (environment.skipFailed) {
+                parseResult
+                    .onFailure { println("Skipping failed `${file.parentFile.name}`") }
+                    .getOrNull()
+            } else parseResult.getOrThrow()
+
+            result.takeUnless { environment.onlyParse }
+        }
+        .forEach { meta ->
+            generateExercise(environment, meta)
+        }
+
+    println()
+    println("Succeeded: ${counters.succeeded.size}")
+    counters.succeeded
+        .takeIf { it.isNotEmpty() }
+        ?.let(::println)
+
+    println("Failed: ${counters.failed.size}")
+    counters.failed
+        .takeIf { it.isNotEmpty() }
+        ?.let(::println)
 
     println("Done!")
 }
@@ -29,5 +61,7 @@ data class EnvironmentCfg(
     val specificationsDir: File,
     val assetsDir: File,
     val outDir: File,
-    val parser: SpecificationsParser = SpecificationsParser()
+    val parser: SpecificationsParser = SpecificationsParser(),
+    val skipFailed: Boolean = false,
+    val onlyParse: Boolean = false
 )
